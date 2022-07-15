02afd188b831e96c1445556c3ab8424523360753
Faisal <faisal.dosani@protonmail.com>
2022-07-15 11:43:33 -0500

    adding in run_update hook (#33)
    * adding in run_update hook and tests
    
    * adding logic for update_with_conda flag for plugin
    
    * fixing docs
    
    * fixing typing from str to Path
    
    * running pip-compile update
    
    * removing extra conda list calls
    
    * Bump 2022.4.1 to 2022.7.0

--- edgetest_conda/plugin.py
+++ edgetest_conda/plugin.py
@@ -2,7 +2,7 @@
 
 import json
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 import pluggy
 from edgetest.logger import get_logger
@@ -23,6 +23,10 @@ def addoption(schema: Schema):
     schema : Schema
         The schema class.
     """
+
+    def to_bool(x):
+        return x.lower() in ["true", "1"]
+
     schema.add_envoption(
         "conda_install",
         {
@@ -35,6 +39,14 @@ def addoption(schema: Schema):
     )
     schema.add_envoption(
         "python_version", {"type": "string", "default": "3.7", "coerce": str}
+    )
+    schema.add_envoption(
+        "update_with_conda",
+        {
+            "type": "boolean",
+            "coerce": to_bool,
+            "required": False,
+        },
     )
 
 
@@ -65,7 +77,7 @@ def _check_mamba() -> bool:
             status = False
     except RuntimeError:
         raise RuntimeError(
-            "Unable to run ``conda list``. Please check that you ``conda`` is available."
+            "Unable to run ``conda list``. Please check that ``conda`` is available."
         )
 
     return status
@@ -83,6 +95,13 @@ def create_environment(basedir: Path, envname: str, conf: Dict):
         The name of the virtual environment.
     conf : dict
         The configuration dictionary for the environment. We will look for ``conda_install``.
+
+    Returns
+    -------
+    bool
+        For ``firstresult`` mark compatability in Pluggy
+        https://pluggy.readthedocs.io/en/stable/index.html#first-result-only
+
 
     Raises
     ------
@@ -112,3 +131,49 @@ def create_environment(basedir: Path, envname: str, conf: Dict):
             "--yes",
         )
         LOG.info(f"Successfully installed conda packages for {envname}")
+
+    return True
+
+
+@hookimpl
+def run_update(basedir: Path, envname: str, upgrade: List, conf: Dict):
+    """Update packages from upgrade list.
+
+    Parameters
+    ----------
+    basedir : Path
+        The base directory location for the environment.
+    envname : str
+        The name of the virtual environment.
+    upgrade : list
+        The list of packages to upgrade
+    conf : dict
+        The configuration dictionary for the environment. We will look for ``update_with_conda``.
+
+    Returns
+    -------
+    bool
+        For ``firstresult`` mark compatability in Pluggy
+        https://pluggy.readthedocs.io/en/stable/index.html#first-result-only
+
+    Raises
+    ------
+    RuntimeError
+        Error raised if the packages cannot be updated.
+    """
+    if conf["update_with_conda"] is False:
+        return None
+
+    env_manager = "mamba" if _check_mamba() else "conda"
+    try:
+        _run_command(
+            env_manager,
+            "update",
+            "-p",
+            str(basedir / envname),
+            *upgrade,
+            "--yes",
+        )
+        return True
+    except Exception:
+        raise RuntimeError(f"Unable to {env_manager} update: {upgrade}")
